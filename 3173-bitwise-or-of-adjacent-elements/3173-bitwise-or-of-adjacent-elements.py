cf3c6178910cf186f34c9ffc61ecf0636a0fbe78
atacanyavuz <43948033+atacanyavuz@users.noreply.github.com>
2024-07-01 08:44:24 +0300

    Time: 96 ms (74.05%), Space: 16.5 MB (46.06%) - LeetHub

--- 3173-bitwise-or-of-adjacent-elements/3173-bitwise-or-of-adjacent-elements.py
+++ 3173-bitwise-or-of-adjacent-elements/3173-bitwise-or-of-adjacent-elements.py
@@ -1,6 +1,6 @@
 class Solution:
     def orArray(self, nums: List[int]) -> List[int]:
-        res = list()
+        res = [0] * (len(nums) - 1)
         for i in range(len(nums) - 1):
-            res.append(nums[i] | nums[i + 1])
+            res[i] = (nums[i] | nums[i + 1])
         return res
