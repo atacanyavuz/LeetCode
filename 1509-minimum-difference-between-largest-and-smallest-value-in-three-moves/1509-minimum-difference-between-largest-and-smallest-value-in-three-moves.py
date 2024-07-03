a0a80c71ab3b1ce18ba483edc83ac4dda69e358e
atacanyavuz <43948033+atacanyavuz@users.noreply.github.com>
2024-07-03 11:49:45 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,18 +1,21 @@
-def qsort(inlist):
-    if inlist == []: 
-        return []
-    else:
-        pivot = inlist[0]
-        lesser = qsort([x for x in inlist[1:] if x < pivot])
-        greater = qsort([x for x in inlist[1:] if x >= pivot])
-        return lesser + [pivot] + greater
+def sort(l):
+    p = 0
+    while p < len(l) - 1:
+        if l[p] > l[p + 1]:
+            l[p], l[p + 1] = l[p + 1], l[p]
+            if not (p == 0):
+                p = p - 1
+        else:
+            p += 1
+    return l
+
 
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
         if len(nums) <= 4:
             return 0
 
-        nums = qsort(nums)
+        nums = sort(nums)
         possibleDiff = [
             nums[-1] - nums[3],
             nums[-2] - nums[2],
