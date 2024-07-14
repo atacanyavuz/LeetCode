ba6c99ef21a65a5d50b809ef70a152e537b8338f
atacanyavuz <43948033+atacanyavuz@users.noreply.github.com>
2024-07-14 11:25:41 +0300

    Time: 43 ms (11.33%), Space: 16.5 MB (85.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,7 +8,7 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = list()
         for item in s:
-            if item in closeParanthesis.keys():
+            if item == "(" or item == "[" or item == "{":
                 stack.append(item)
             else:
                 if len(stack) == 0 or not closeParanthesis[stack.pop()] == item:
