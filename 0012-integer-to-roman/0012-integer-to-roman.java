7cf3f43581c491a50ed0ba84a35e53fccbf1c76b
atacanyavuz <43948033+atacanyavuz@users.noreply.github.com>
2025-03-06 12:03:04 +0300

    Time: 3 ms (98.23%), Space: 44.5 MB (49.96%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,30 +1,18 @@
 class Solution {
-    private void fillRomanMap(LinkedHashMap<Integer, String> romanMap){
-        romanMap.put(1000, "M");
-        romanMap.put(900, "CM");
-        romanMap.put(500, "D");
-        romanMap.put(400, "CD");
-        romanMap.put(100, "C");
-        romanMap.put(90, "XC");
-        romanMap.put(50, "L");
-        romanMap.put(40, "XL");
-        romanMap.put(10, "X");
-        romanMap.put(9, "IX");
-        romanMap.put(5, "V");
-        romanMap.put(4, "IV");
-        romanMap.put(1, "I");
-    }
+    private static final int[] numbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] romanValue = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
     public String intToRoman(int num) {
         StringBuilder result = new StringBuilder();
-        LinkedHashMap<Integer, String> romanMap = new LinkedHashMap<>();
-        fillRomanMap(romanMap);
-        for (int key : romanMap.sequencedKeySet()) {
-            while (num >= key) {
-                result.append(romanMap.get(key));
-                num = num - key;
+        int index = 0;
+        while (num != 0) {
+            while (num >= numbers[index]) {
+                result.append(romanValue[index]);
+                num -= numbers[index];
             }
+            index++;
         }
+
         return result.toString();
     }
 }
