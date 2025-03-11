0254e705ba9fa550274fe93c1731623cc18674f8
atacanyavuz <43948033+atacanyavuz@users.noreply.github.com>
2025-03-11 10:29:47 +0300

    Time: 18 ms (25.14%), Space: 44.6 MB (64.19%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,27 +1,5 @@
 class Solution {
     int[] charCounter = new int[3];
-
-    private void addRemoveCharToCounter(char c, boolean isRemove) {
-        if (isRemove) {
-            if (c == 'a') {
-                charCounter[0]--;
-            } else if (c == 'b') {
-                charCounter[1]--;
-            } else if (c == 'c') {
-                charCounter[2]--;
-            }
-        }
-        else {
-            if (c == 'a') {
-                charCounter[0]++;
-            } else if (c == 'b') {
-                charCounter[1]++;
-            } else if (c == 'c') {
-                charCounter[2]++;
-            }
-        }
-
-    }
     private boolean controlCounter(){
         return charCounter[0] > 0 && charCounter[1] > 0 && charCounter[2] > 0; 
     }
@@ -32,7 +10,6 @@ class Solution {
         int rightIndex = 0;
         while (rightIndex < s.length()) {
             charCounter[s.charAt(rightIndex) - 'a']++;
-            System.out.println(charCounter[0] + "-" + charCounter[1] + "-" + charCounter[2] );
             while (controlCounter()) {
                 totalNumber += (s.length() - rightIndex);
                 charCounter[s.charAt(leftIndex) - 'a']--;
